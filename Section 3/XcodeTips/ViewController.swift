3b60478add54125968048466cc735087bbfe5650
harsh-packt <harshg@packtpub.com>
2018-10-11 13:10:01 +0530

    Code Added
    Code Added

---
+++ Section 3/XcodeTips/ViewController.swift
@@ -0,0 +1,30 @@
+//ViewController.swift
+//XcodeTips 
+	
+//Created by Dee Odus.
+//Copyright Dee Odus (Appkoder.com). All Rights Reserved.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("GitLab project")
+        print("New project")
+    }
+    
+    func new(){
+        
+        print("function")
+        
+    }
+    
+    func another(){
+        
+    }
+
+
+}
+
