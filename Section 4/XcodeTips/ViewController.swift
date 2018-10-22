a30807ca80e3e3fc56f9c931f53558eb34e0860a
harsh-packt <harshg@packtpub.com>
2018-10-22 19:06:48 +0530

    Code added
    Code added

---
+++ Section 4/XcodeTips/ViewController.swift
@@ -0,0 +1,20 @@
+//ViewController.swift
+//XcodeTips 
+	
+//Created by Dee Odus.
+//Copyright Dee Odus (Appkoder.com). All Rights Reserved.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
